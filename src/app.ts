82f6ae1357213a7faa583fc313ded28c9a1b81f5
mohit-sonii <mohitsoni9731@gmail.com>
2025-05-21 23:15:07 +0530

    Initialised the Project

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+
+app.listen(process.env.PORT, () => {
+    console.log(`Application is running on PORT: ${process.env.PORT}`)
+})
+
+
+export { app }
